67a8c328042f217396bc6f023eedc66e39fb8f84
kerdemdemir <kerdemdemir@gmail.com>
2020-09-20 11:43:24 +0200

    Simplified the project like crazy . More data .

--- scikitBot.py
+++ scikitBot.py
@@ -30,12 +30,8 @@ totalUsedCurveCount = 4
 isConcanateCsv = False
 acceptedProbibilty = 0.9
 testRatio = 4
-transParamList = [inputManager.TransactionParam(250,  10),
-                  inputManager.TransactionParam(500,  10),
-                  inputManager.TransactionParam(750,  10),
-                  inputManager.TransactionParam(1000, 10),
-                  inputManager.TransactionParam(1500, 10),
-                  inputManager.TransactionParam(2500, 10)]
+transParamList = [inputManager.TransactionParam(250,  100)]
+
 currentProbs = []
 
 
@@ -44,10 +40,6 @@ def ReadFileAndCreateReshaper( fileName ):
     file = open(fileName, "r")
     jsonDictionary = json.load(file)
 
-    # for transactionBinCount in transactionBinCountList:
-    #     for index in range(totalTimeCount):
-    #         transactionParam = inputManager.TransactionParam(DynamicTuner.startTime + DynamicTuner.smallestTime * (index * 2 + 1), transactionBinCount)
-    #         transParamList.append(transactionParam)
     reshaper = inputManager.ReShapeManager(transParamList)
 
     for jsonElem in jsonDictionary:
@@ -62,35 +54,7 @@ def AddExtraToTuneShaper ( fileName, shaper):
         for jsonElem in jsonDictionary:
             shaper.addANewCurrency(jsonElem, True)
     except:
-        file = open(os.path.abspath(os.getcwd()) + "/Data/TuneData/" + fileName, "r")
-        temp = file.readline()
-        startIndex = 0
-        curCount = 0
-        isAlert = False
-        for index in range(len(temp)):
-            if temp[index] == "{":
-                curCount += 1
-                if curCount == 1:
-                    startIndex = index
-            elif temp[index] == "}" :
-                curCount -= 1
-                if curCount == 0 :
-                    jsonStr = temp[startIndex:index + 1]
-                    if  temp[index - 1] == "]":
-                        #print(jsonStr)
-                        if not isAlert:
-                            try:
-                                jsonElem = json.loads( jsonStr )
-                                shaper.addANewCurrency(jsonElem, True)
-                            except:
-                                print("Another exception")
-                                continue
-                        else:
-                            isAlert = False
-                    else:
-                        curCount = 1
-                        isAlert = True
-                        print(isAlert, " ", jsonStr)
+        print("There was a exception in ", fileName)
 
 def ReadFilesInTuneFolder( folderPath, reshaperTuner ):
     onlyfiles = [f for f in listdir(folderPath) if isfile(join(folderPath, f))]
@@ -107,41 +71,10 @@ def AddExtraToShaper ( fileName, shaper, IsTransactionOnly):
         for jsonElem in jsonDictionary:
             shaper.addANewCurrency(jsonElem, IsTransactionOnly)
     except:
-        file = open(fileName, "r")
-        temp = file.readline()
-        startIndex = 0
-        curCount = 0
-        isAlert = False
-        for index in range(len(temp)):
-            if temp[index] == "{":
-                curCount += 1
-                if curCount == 1:
-                    startIndex = index
-            elif temp[index] == "}" :
-                curCount -= 1
-                if curCount == 0 :
-                    jsonStr = temp[startIndex:index + 1]
-                    if  temp[index - 1] == "]":
-                        #print(jsonStr)
-                        if not isAlert:
-                            try:
-                                jsonElem = json.loads( jsonStr )
-                                shaper.addANewCurrency(jsonElem, IsTransactionOnly)
-                            except:
-                                print("Another exception")
-                                continue
-                        else:
-                            isAlert = False
-                    else:
-                        curCount = 1
-                        isAlert = True
-                        print(isAlert, " ", jsonStr)
-
-
-
+        print("There was a exception in ", fileName)
     file.close()
 
-def Predict ( messageChangeTimeTransactionStrList, mlpTransactionScalerList, mlpTransactionList, mlpScalerList, mlpList, mixTransactionLearner ):
+def Predict ( messageChangeTimeTransactionStrList, mlpTransactionScalerList, mlpTransactionList, trainingReshaper):
     priceStrList = messageChangeTimeTransactionStrList[0].split(",")
     timeStrList = messageChangeTimeTransactionStrList[1].split(",")
     transactionStrList = messageChangeTimeTransactionStrList[2].split(",")
@@ -155,7 +88,7 @@ def Predict ( messageChangeTimeTransactionStrList, mlpTransactionScalerList, mlp
         extraStuff = resultsTransactionFloat[-transHelper.ExtraFeatureCount:]
         justTransactions = resultsTransactionFloat[:-transHelper.ExtraFeatureCount]
         currentTransactionList = DynamicTuner.MergeTransactions(justTransactions, transParam.msec, transParam.gramCount)
-        scores = reshaperTuner.getScoreList(resultsChangeFloat)
+        scores = trainingReshaper.getScoreList(resultsChangeFloat)
 
         totalFeatures = currentTransactionList + extraStuff + resultsTimeFloat[-3:] + scores
         totalFeaturesNumpy = np.array(totalFeatures).reshape(1, -1)
@@ -167,27 +100,6 @@ def Predict ( messageChangeTimeTransactionStrList, mlpTransactionScalerList, mlp
         curResultStr = str(predict_test) + ";"
         resultStr += curResultStr
 
-    totalPredict = []
-    for binCount in range(inputManager.ReShapeManager.maxFeatureCount - inputManager.ReShapeManager.minFeatureCount - 1):
-        curCount = binCount + inputManager.ReShapeManager.minFeatureCount
-        totalCurves = resultsChangeFloat[-curCount:] + resultsTimeFloat[-curCount:]
-        npTotalCurves = np.array(totalCurves)
-        npTotalCurves = npTotalCurves.reshape(1, -1)
-        npTotalCurvesScaled = mlpScalerList[binCount].transform(npTotalCurves)
-        print("I will predict the curves: ", totalCurves)
-        predict_test = mlpList[binCount].predict_proba(npTotalCurvesScaled)
-        curResultStr = str(predict_test) + ";"
-        resultStr += curResultStr
-        predict_test = np.delete(predict_test, 0, 1)
-        if binCount < totalUsedCurveCount:
-            totalPredict = np.append(totalPredict, predict_test)
-
-    resultStr = resultStr[:-1]
-    totalPredict = totalPredict.reshape(1, -1)
-    print("I will predict the fusion: ", totalPredict)
-    totalPredictResult = mixTransactionLearner.predict_proba(totalPredict)
-    totalPredictResultStr = str(totalPredictResult) + ";"
-    resultStr = totalPredictResultStr + resultStr
     print("Results are: ", resultStr)
     return resultStr
 
@@ -221,33 +133,11 @@ for fileName in onlyfiles:
 print("All added now scores")
 #trainingReshaper.transactionHelper.Print()
 a = datetime.datetime.now()
-if isTrainCurves:
-    trainingReshaper.assignScores()
+trainingReshaper.assignScores()
 b = datetime.datetime.now()
 elapsedTime = b - a
 print("Assigned scores ", elapsedTime.seconds)
 sys.stdout.flush()
-
-mlpList = [[] for _ in range(inputManager.ReShapeManager.maxFeatureCount - inputManager.ReShapeManager.minFeatureCount)]
-mlpScalerList = [[] for _ in range(inputManager.ReShapeManager.maxFeatureCount - inputManager.ReShapeManager.minFeatureCount)]
-
-if isTrainCurves :
-    for binCount in range (inputManager.ReShapeManager.minFeatureCount, inputManager.ReShapeManager.maxFeatureCount):
-        curIndex = binCount - inputManager.ReShapeManager.minFeatureCount
-        numpyArr = trainingReshaper.toFeaturesNumpy(binCount)
-
-        mlpScalerList[curIndex] = preprocessing.StandardScaler().fit(numpyArr)
-        X = mlpScalerList[curIndex].transform(numpyArr)
-        y = trainingReshaper.toResultsNumpy(binCount)
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=40)
-        y_test = trainingReshaper.toTestResultNumpy(X_test,binCount)
-        nodeSize = min(8, binCount*2)
-        mlpList[curIndex] = MLPClassifier(hidden_layer_sizes=(nodeSize,nodeSize,nodeSize), activation='relu', solver='adam', max_iter=500)
-        mlpList[curIndex].fit(X_train,y_train)
-        predict_test = mlpList[curIndex].predict(X_test)
-        print( " Curves : ")
-        print( confusion_matrix(y_test,predict_test))
-        sys.stdout.flush()
 
 mlpTransactionList = []
 mlpTransactionScalerList = []
@@ -273,96 +163,12 @@ for transactionIndex in range(len(transParamList)):
     print(" Transactions time: ", transParam.msec, " Transaction Index ", transParam.gramCount, "Index ", transactionIndex)
     print(confusion_matrix(y_test, finalResult))
 
-resultPredicts = [[] for _ in range(inputManager.ReShapeManager.maxFeatureCount - 1 - inputManager.ReShapeManager.minFeatureCount)]
-mixTransactionLearner = MLPClassifier(hidden_layer_sizes=(4, 4, 4), activation='relu',
-                                      solver='adam', max_iter=500)
-
-
-
-
-
-print("Start Tuning")
-reshaperTuner = inputManager.ReShapeManager([inputManager.TransactionParam(DynamicTuner.smallestTime,DynamicTuner.totalTransactions)])
-ReadFilesInTuneFolder( os.path.abspath(os.getcwd()) + "/Data/TuneData/", reshaperTuner )
-reshaperTuner.assignScores()
-transactionTuner = DynamicTuner.PeakTransactionTurner(len(transParamList))
-transactionTuner.Init(reshaperTuner, mlpTransactionScalerList, mlpTransactionList,transParamList)
-
-
-curves = reshaperTuner.toTransactionCurves(0)
-goodCount = 0
-goodNegativeCount = 0
-badCount = 0
-badNegativeCount = 0
-for index in range(len(curves)):
-    elem = curves[index]
-    isButtom = elem[-1] < 0.0
-    score = trainingReshaper.getScore(elem)
-    reshaperTuner.setScore(0, index, score)
-    if score > 10.0:
-        if isButtom:
-            goodCount+=1
-            #print("Good ", elem)
-        else:
-            goodNegativeCount+=1
-    elif score < -1.0:
-        if isButtom:
-            #print("Was good but eliminated", elem)
-            badCount+=1
-        else:
-            badNegativeCount+=1
-print( " Good ", goodCount, " Negative good ", goodNegativeCount, " Bad ", badCount, " Bad negative ", badNegativeCount, " Total ", len(curves))
-
-print("Score 0" )
-transactionTuner.InitWithScore(reshaperTuner, mlpTransactionScalerList, mlpTransactionList,transParamList, 0)
-print("Score 8" )
-transactionTuner.InitWithScore(reshaperTuner, mlpTransactionScalerList, mlpTransactionList,transParamList, 8)
-print("Score 7" )
-transactionTuner.InitWithScore(reshaperTuner, mlpTransactionScalerList, mlpTransactionList,transParamList, 7)
-print("Score 10" )
-transactionTuner.InitWithScore(reshaperTuner, mlpTransactionScalerList, mlpTransactionList,transParamList, 10)
-print("Score 3" )
-transactionTuner.InitWithScore(reshaperTuner, mlpTransactionScalerList, mlpTransactionList,transParamList, 3)
-print("Score 6" )
-transactionTuner.InitWithScore(reshaperTuner, mlpTransactionScalerList, mlpTransactionList,transParamList, 6)
-print("Score -3" )
-transactionTuner.InitWithScore(reshaperTuner, mlpTransactionScalerList, mlpTransactionList,transParamList, -3)
-print("Score -6" )
-transactionTuner.InitWithScore(reshaperTuner, mlpTransactionScalerList, mlpTransactionList,transParamList, -6)
-print("Score -10" )
-transactionTuner.InitWithScore(reshaperTuner, mlpTransactionScalerList, mlpTransactionList,transParamList, -10)
-
 trainingReshaper.ClearMemory()
 
-if isTrainCurves:
-    for binCount in range (inputManager.ReShapeManager.minFeatureCount, inputManager.ReShapeManager.maxFeatureCount-1):
-        curIndex = binCount - inputManager.ReShapeManager.minFeatureCount
-        numpyArr = reshaperTuner.toTransactionCurvesToNumpy(0, binCount)
-        X = mlpScalerList[curIndex].transform(numpyArr)
-        curResultPredict = mlpList[curIndex].predict_proba(X)
-        resultPredicts[curIndex] = np.delete(curResultPredict, 0 , 1 )
-        sys.stdout.flush()
-    y = reshaperTuner.toTransactionPeakResultsNumpy(0)
-    mergedArray = np.concatenate((resultPredicts[0], resultPredicts[1], resultPredicts[2], resultPredicts[3]), axis=1)
-    X_trainMearged, X_testMerged, y_trainMerged, y_testMerged = train_test_split(mergedArray, y, test_size=0.2, random_state=40)
-    print(" Transactions and curves merged: ")
-    DynamicTuner.FitPredictAndPrint(mixTransactionLearner, X_trainMearged, X_testMerged, y_trainMerged, y_testMerged)
-    print(" Tuning the curves ")
-    finalResultNew = [0.0] * mergedArray.shape[1]
-    DynamicTuner.AdjustTheBestCurve(mergedArray, y, finalResultNew)
-    currentProbs = transactionTuner.finalResult + finalResultNew
-    print( " Tuning result is ", finalResultNew)
 
 currentProbs = [0.5, 0.5] + currentProbs
 print( " Current tuned probibilities are : ", currentProbs)
 sys.stdout.flush()
-
-# print("Start Short memory tuning")
-# shortMemReshaperTuner = inputManager.ReShapeManager([inputManager.TransactionParam(125,80)])
-# AddExtraToTuneShaper("/Data/TuneData/learning_07_08.txt", shortMemReshaperTuner)
-# transactionShortMemTuner = DynamicTuner.PeakTransactionTurner(len(transParamList))
-# transactionShortMemTuner.Init(shortMemReshaperTuner, mlpTransactionScalerList, mlpTransactionList,transParamList)
-
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
@@ -376,41 +182,18 @@ while True:
 
     if command == "Predict":
         messageChangeTimeTransactionStrList = messageChangeTimeTransactionStrList[1:]
-        resultStr = Predict(messageChangeTimeTransactionStrList, mlpTransactionScalerList, mlpTransactionList, mlpScalerList, mlpList, mixTransactionLearner)
-        resultsTransactionFloat = [float(transactionResults[2:-2].split(" ")[1]) for transactionResults in resultStr.split(";")[1:len(transParamList) + 1]]
-        print("Trasaction fusion will be asked: ", resultsTransactionFloat)
-        tunerResult = transactionTuner.GetResult(resultsTransactionFloat)
-        resultStr = tunerResult + ";" + resultStr
-
-        # shortTunerResult = transactionShortMemTuner.GetResult(resultsTransactionFloat)
-        # resultStr = shortTunerResult + ";" + resultStr
-
+        resultStr = Predict(messageChangeTimeTransactionStrList, mlpTransactionScalerList, mlpTransactionList, trainingReshaper)
         print("Results are: ", resultStr)
         #  Send reply back to client
         socket.send_string(resultStr, encoding='ascii')
         sys.stdout.flush()
-    elif command == "Train":
-        isBottom = messageChangeTimeTransactionStrList[1] == "Bottom"
-        messageChangeTimeTransactionStrList = messageChangeTimeTransactionStrList[2:]
-        reJoinedMessageStr = ";".join(messageChangeTimeTransactionStrList)
-        requestList = reJoinedMessageStr.split("|")
-        isReTrain = False
-        for request in requestList:
-            print("Training predictions for : ", request )
-            requestSplitedList = request.split(";")
-            resultStr = Predict(requestSplitedList, mlpTransactionScalerList, mlpTransactionList, mlpScalerList, mlpList, mixTransactionLearner)
-            isReTrain |= transactionTuner.Add(isBottom, resultStr)
-        socket.send_string("Done", encoding='ascii')
-        if isReTrain:
-            transactionTuner.Train()
-        sys.stdout.flush()
-    if command == "GetScore":
+    elif command == "GetScore":
         priceStrList = messageChangeTimeTransactionStrList[1].split(",")
         resultsChangeFloat = [float(messageStr) for messageStr in priceStrList]
         score = trainingReshaper.getScore(resultsChangeFloat)
         print( " Score for list: ", priceStrList, " is ", score)
         socket.send_string(str(score), encoding='ascii')
-    if command == "Adjust":
+    elif command == "Adjust":
         print( " I will send back the new probabilities ", currentProbs)
         socket.send_string(str(currentProbs), encoding='ascii')
 
